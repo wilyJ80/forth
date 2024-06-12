96e40a1b7b996dead14339402a7020334c438ef2
wilyJ80 <abjurandam@gmail.com>
2024-06-12 14:42:45 -0300

    Set up baseline layout

---
+++ jsforth/forth.js
@@ -0,0 +1,111 @@
+module.exports = class Forth {
+	constructor() {
+		this.dataStack = [];
+		this.returnStack = [];
+		this.dictionary = {};
+	}
+
+	push = (value) => {
+		this.dataStack.push(value);
+	};
+
+	pop = () => {
+		if (this.dataStack.length === 0) {
+			throw new Error("Stack underflow");
+		}
+		return this.dataStack.pop();
+	};
+
+	defineWord = (name, func) => {
+		this.dictionary[name] = func;
+	};
+
+	executeWord = (name) => {
+		if (name in this.dictionary) {
+			this.dictionary[name].call(this);
+		} else {
+			throw new Error(`Undefined word: ${name}`);
+		}
+	};
+
+	initCoreWords = () => {
+		this.defineWord('+', () => {
+			const b = this.pop();
+			const a = this.pop();
+			this.push(a + b);
+		});
+
+		this.defineWord('-', () => {
+			const b = this.pop();
+			const a = this.pop();
+			this.push(a - b);
+		});
+
+		this.defineWord('*', () => {
+			const b = this.pop();
+			const a = this.pop();
+			this.push(a * b);
+		});
+
+		this.defineWord('/', () => {
+			const b = this.pop();
+			const a = this.pop();
+			this.push(a / b);
+		});
+
+		this.defineWord('.', () => {
+			// TODO: may need to change this for web interface
+			console.log(this.pop());
+		});
+
+		this.defineWord('drop', () => {
+			this.pop();
+		});
+
+		this.defineWord('dup', () => {
+			const a = this.pop();
+			this.push(a);
+			this.push(a);
+		});
+
+		this.defineWord('swap', () => {
+			const b = this.pop();
+			const a = this.pop();
+			this.push(b);
+			this.push(a);
+		});
+
+		this.defineWord('over', () => {
+			const b = this.pop();
+			const a = this.pop();
+			this.push(b);
+			this.push(a);
+			this.push(b);
+		});
+
+		this.defineWord('rot', () => {
+			const c = this.pop();
+			const b = this.pop();
+			const a = this.pop();
+			this.push(b);
+			this.push(c);
+			this.push(a);
+		});
+
+		this.defineWord('words', () => {
+			// TODO: may need to change this for web interface
+			console.log(Object.keys(this.dictionary));
+		});
+	};
+
+	evaluate = (input) => {
+		const tokens = input.split(/\s+/).filter(token => token.length > 0);
+		for (const token of tokens) {
+			if (!isNaN(token)) {
+				this.push(parseFloat(token));
+			} else {
+				this.executeWord(token);
+			}
+		}
+	};
+}
